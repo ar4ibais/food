e7cb8ec280c4c8a31d69cbab1062292386b2db7e
ar4ibais <ak.hutov@yandex.ru>
2023-07-31 16:25:45 +0400

    installed json-server and initial db.json how a DB for our get- and push- functions

--- js/script.js
+++ js/script.js
@@ -160,7 +160,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         changeToUAH() {
-            this.price = Math.floor(this.price / this.transfer);
+            this.price = Math.floor(this.price * this.transfer);
         }
 
         render() {
@@ -180,41 +180,30 @@ window.addEventListener('DOMContentLoaded', () => {
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> $/день</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div> 
             `;
 
             this.parent.append(elem);
         }
-    }
+    };
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        'vegy',
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        229,
-        '.menu__field .container'
-    ).render();
+    const getResource = async (url) => {
+        const res =  await fetch(url);
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        'elite',
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        550,
-        '.menu__field .container'
-    ).render();
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        'post',
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        430,
-        '.menu__field .container'
-    ).render();
+        return await res.json();
+    };
 
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            })
+        })
 
     // Forms
 
@@ -227,10 +216,22 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(form => {
-        postData(form);
+        bindPostData(form);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res =  await fetch(url, {
+            method: 'POST', 
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', e => {
             e.preventDefault();
 
@@ -245,21 +246,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: 'POST', 
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-                .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
                 .then(data => {
-                    console.log(data);
                     showThanksModal(message.success);
                     statusMessage.remove();
                 })
